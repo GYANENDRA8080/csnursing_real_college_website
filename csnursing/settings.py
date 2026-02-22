c859b2ca9f7fdd9af78520791725ecc8e08c5b08
GYANENDRA8080 <gyanendra8080@gmail.com>
2026-02-22 21:39:42 +0530

    admission22

--- csnursing/settings.py
+++ csnursing/settings.py
@@ -4,7 +4,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'real-college-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.31.238', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
